337208adfe6cd66ce8b8f95ee8637418b0f835e4
Akayy <techakayy@gmail.com>
2023-09-13 04:52:41 +1000

    Minor updates

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,8 @@
     <v-main>
       <div>
         <MetaTags />
-        <div
-          class="flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50"
-        >
-          <!-- <NavBar class="fixed bg-white dark:bg-neutral-950 shadow z-10" /> -->
+        <div class="d-flex flex-column bg-background text-on-background">
+          <!-- <NavBar class="position-fixed bg-background shadow" style="z-index: 10;" /> -->
           <!-- <div class="mt-24 shadow"> -->
           <slot />
           <!-- </div> -->
