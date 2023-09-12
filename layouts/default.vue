81246a07c81176d16e596c40ecf13e6eeb936fc3
Akayy <techakayy@gmail.com>
2023-09-13 01:59:32 +1000

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <div>
+        <MetaTags />
+        <div
+          class="flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50"
+        >
+          <!-- <NavBar class="fixed bg-white dark:bg-neutral-950 shadow z-10" /> -->
+          <!-- <div class="mt-24 shadow"> -->
+          <slot />
+          <!-- </div> -->
+          <!-- <TheFooter /> -->
+        </div>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  definePageMeta({
+    title: 'Home',
+    navOrder: 1,
+  })
+
+  useHead({
+    title: () => 'Home',
+  })
+</script>
+<template>
+  <main>
+    <SampleCard />
+  </main>
+</template>
+<style scoped></style>
