f0500b0689a57f80d43aef80e2bc757c53fb8cc7
Akayy <techakayy@gmail.com>
2023-09-22 06:05:48 +1000

    Image optimizations

--- components/SampleCard.vue
+++ components/SampleCard.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
-  import { useHero } from '@/composables/hero'
-  const { heroImage, heroLazySrc, heroSrc } = useHero()
+  import { pg_background_urls } from '~~/themes/pg-vuetify/tokens.mjs'
+
+  const { optimizeImage, optimizeImages } = useOptimizeImage()
+
+  const heroImageUrl =
+    pg_background_urls['design-image-large'] ||
+    pg_background_urls['design-image']
+
+  const heroImageOptimized = optimizeImage(heroImageUrl)
 
   const availableTimeSlots = ['3.30PM', '4.20PM', '5.50PM', '6.00PM']
   const selectedTimeSlot = ref(0)
@@ -13,12 +20,11 @@
 <template>
   <v-card width="360" class="mt-12 mx-auto" elevation="4">
     <v-img
-      :src="heroSrc"
+      :src="heroImageOptimized.imageSrc"
+      :srcset="heroImageOptimized.imageSizes.srcset"
+      :sizes="heroImageOptimized.imageSizes.sizes"
       height="250"
       cover
-      :lazy-src="heroLazySrc"
-      :srcset="heroImage.srcset"
-      :sizes="heroImage.sizes"
     ></v-img>
     <v-card-item>
       <template #title>
@@ -27,8 +33,8 @@
           <DarkModeSwitch />
         </div>
       </template>
-      <template #subtitle>
-        <span>Local Favourite</span>
+      <template #subtitle
+        ><span>Local Favourite</span>
         <v-icon
           icon="i-ic-round-local-fire-department"
           class="bg-red-darken-4 ml-2 text-red-darken-2"
@@ -42,12 +48,14 @@
           color="amber-darken-2"
           model-value="4.5"
           half-increments
-        ></v-rating>
-        <span class="ml-2 mt-2 text-caption text-medium-emphasis"
+        ></v-rating
+        ><span class="ml-2 mt-2 text-caption text-medium-emphasis"
           >4.5 (413)</span
         >
       </div>
-      <div class="mt-2 text-subtitle-1"><span>$ . Italian, Cafe</span></div>
+      <div class="mt-2 text-subtitle-1">
+        <span>$ . Italian, Cafe</span>
+      </div>
     </v-card-item>
     <v-card-text>
       <div class="mt-1">
@@ -59,9 +67,7 @@
       <v-divider class="mt-4"></v-divider>
     </v-card-text>
     <v-card-item>
-      <template #title>
-        <span>Tonight's availability</span>
-      </template>
+      <template #title><span>Tonight's availability</span> </template>
       <v-chip-group v-model="selectedTimeSlot" class="mt-2">
         <v-chip
           v-for="(timeSlot, index) in availableTimeSlots"
@@ -77,8 +83,8 @@
         class="mb-2 ml-2"
         @click="showStatus(true)"
       ></v-btn>
-      <v-snackbar v-model="statusDisplay" timeout="2000">
-        <span class="text-white w-100"
+      <v-snackbar v-model="statusDisplay" timeout="2000"
+        ><span class="text-white w-100"
           >{{ availableTimeSlots[selectedTimeSlot] }} was reserved.</span
         >
       </v-snackbar>
