3af940897e97ea85da9244d83f4ab0836d09df6d
Akayy <techakayy@gmail.com>
2025-01-14 21:36:15 +1100

    Updates, navbar refactor and head-and-meta.ts

--- server/api/store/local-data/index.ts
+++ server/api/store/local-data/index.ts
@@ -1,13 +1,22 @@
-import type { Store, Products } from '~~/types/store'
-import products from './data'
+import type { Store, Product } from '~~/types/store'
+import productsRaw from './data'
 
-products as Products
+const products: Product[] = productsRaw
 
 export default defineEventHandler(async (/*event*/): Promise<Store> => {
   const categories = [...new Set(products.map((product) => product.category))]
 
+  const badges = [
+    ...new Set(
+      products
+        .filter((product) => product.badge)
+        .map((product) => product.badge),
+    ),
+  ]
+
   return {
     products,
     categories,
+    badges,
   }
 })
